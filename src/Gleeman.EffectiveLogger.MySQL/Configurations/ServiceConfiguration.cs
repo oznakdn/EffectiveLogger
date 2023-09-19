85ea6783cb6ffb037144d4db06b56d11927e069b
oznakdn <79724084+oznakdn@users.noreply.github.com>
2023-09-19 09:43:14 +0300

    Logging to MySQL package project created

---
+++ src/Gleeman.EffectiveLogger.MySQL/Configurations/ServiceConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace Gleeman.EffectiveLogger.MySQL.Configurations;
+
+public static class ServiceConfiguration
+{
+    public static IServiceCollection AddMySqlLog(this IServiceCollection services, IConfiguration configuration, Assembly assembly)
+    {
+
+        DbContextOptionsBuilder builder = new DbContextOptionsBuilder();
+        string connectionString = configuration.GetValue<string>("LogOptions:DatabaseOptions:MySqlConnectionString")!;
+        services.AddDbContext<LogContext>(option => option.UseMySql(connectionString,ServerVersion.AutoDetect(connectionString), x => x.MigrationsAssembly(assembly.FullName)));
+        services.AddEffectiveLogger(configuration);
+        return services;
+    }
+}
---
+++ src/Gleeman.EffectiveLogger.MySQL/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using Gleeman.EffectiveLogger.Context;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using System.Reflection;
+global using Gleeman.EffectiveLogger.Configuration;
